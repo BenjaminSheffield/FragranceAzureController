86d6e0d650eaf7ac97f9f279efcdc51803754616
Benjamin Sheffield <benjaminsheffield@live.com>
2015-08-18 17:24:34 +1200

    init commit

---
+++ FragranceDatabaseController/FragranceDatabaseController/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FragranceDatabaseController.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ FragranceDatabaseController/FragranceDatabaseController/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FragranceDatabaseController.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
