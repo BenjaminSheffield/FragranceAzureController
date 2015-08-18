94541a28f1e753ed5e59ad45b4476b077c040c8e
Benjamin Sheffield <benjaminsheffield@live.com>
2015-08-19 00:30:19 +1200

    Fragrance Controller

---
+++ FragranceController/FragranceController/Controllers/FragrancesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FragranceController;
+
+namespace FragranceController.Controllers
+{
+    public class FragrancesController : ApiController
+    {
+        private FragranceDbAccess db = new FragranceDbAccess();
+
+        // GET: api/Fragrances
+        public IQueryable<Fragrance> GetFragrances()
+        {
+            return db.Fragrances;
+        }
+
+        // GET: api/Fragrances/5
+        [ResponseType(typeof(Fragrance))]
+        public IHttpActionResult GetFragrance(int id)
+        {
+            Fragrance fragrance = db.Fragrances.Find(id);
+            if (fragrance == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(fragrance);
+        }
+
+        // PUT: api/Fragrances/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutFragrance(int id, Fragrance fragrance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != fragrance.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(fragrance).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FragranceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Fragrances
+        [ResponseType(typeof(Fragrance))]
+        public IHttpActionResult PostFragrance(Fragrance fragrance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Fragrances.Add(fragrance);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = fragrance.id }, fragrance);
+        }
+
+        // DELETE: api/Fragrances/5
+        [ResponseType(typeof(Fragrance))]
+        public IHttpActionResult DeleteFragrance(int id)
+        {
+            Fragrance fragrance = db.Fragrances.Find(id);
+            if (fragrance == null)
+            {
+                return NotFound();
+            }
+
+            db.Fragrances.Remove(fragrance);
+            db.SaveChanges();
+
+            return Ok(fragrance);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FragranceExists(int id)
+        {
+            return db.Fragrances.Count(e => e.id == id) > 0;
+        }
+    }
+}
